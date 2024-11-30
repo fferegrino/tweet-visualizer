c64bf3ed8530af4f543d4cd8d32d58185fd2b05f
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-11-30 09:27:36 +0000

    Upgrade

--- src/plots/GitHub.js
+++ src/plots/GitHub.js
@@ -9,75 +9,38 @@ function GitHub({ tweetTimes, markerSize, plotWidth, plotHeight, timeZone }) {
       const now = new Date();
       // Get the last day of the week
       const lastDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + (6 - now.getDay()));
-      const oneYearAgo = new Date(lastDay.getFullYear() - 1, lastDay.getMonth(), lastDay.getDate());
-      const weeksBetween = Math.floor((lastDay - oneYearAgo) / (7 * 24 * 60 * 60 * 1000));
-      const daysOfYear = Array.from({ length: 7 }, () => Array.from({ length: weeksBetween + 1 }, () => 0));
-      console.log(
-        'Last day:', lastDay,
-        'One year ago:', oneYearAgo,
-        'Weeks between:', weeksBetween,
-        'Days of year:', daysOfYear
-      ) 
+      const oneYearAgo = new Date(lastDay.getFullYear()-1, lastDay.getMonth(), lastDay.getDate());
+      const lastDayOneYearAgo = new Date(oneYearAgo.getFullYear(), oneYearAgo.getMonth(), oneYearAgo.getDate() + (6 - oneYearAgo.getDay()));
+      const firstDayOneYearAgo = new Date(oneYearAgo.getFullYear(), oneYearAgo.getMonth(), oneYearAgo.getDate() - oneYearAgo.getDay() - 1);
+      const weeksBetween = Math.floor((lastDay - lastDayOneYearAgo) / (7 * 24 * 60 * 60 * 1000));
+      const daysOfYear = Array.from({ length: 7 }, () => Array.from({ length: weeksBetween }, () => 0));
+
 
       const periodInMinutes = 60;
       const minutesInDay = 24 * 60;
       const periodCount = minutesInDay / periodInMinutes;
 
-
-      let maxCount = 0;
-
       for (const date of tweetTimes) {
         const dt = new Date(date);
         const dayOfWeek= dt.getDay();
-        const weekOfYear = Math.floor((dt - oneYearAgo) / (7 * 24 * 60 * 60 * 1000));
+        const weekOfYear = Math.floor((dt - firstDayOneYearAgo) / (7 * 24 * 60 * 60 * 1000)) - 1;
         daysOfYear[dayOfWeek][weekOfYear]++;
 
       }
 
-
-      const xMargin = 0.5;
-      const yMargin = 0.5;
-
-      // const daysAsStrings = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       const daysAsStrings = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
       const periods = Array.from({ length: periodCount }, (_, i) => i);
-      const periodsAsStrings = periods.map((_, i) => {
-        if ( i % 6 !== 0) {
-          return '';
-        }
-        const startMinute = i * periodInMinutes;
-        const endMinute = (i + 1) * periodInMinutes;
-        let startHour = Math.floor(startMinute / 60);
-        const startMinuteInHour = startMinute % 60;
-        const endHour = Math.floor(endMinute / 60);
-        const endMinuteInHour = endMinute % 60;
-
-        let suffix = 'AM';
-        if (startHour >= 12) {
-          suffix = 'PM';
-          if (startHour > 12) {
-            startHour -= 12;
-          }
-        }
-        
-        return `${startHour}:${startMinuteInHour.toString().padStart(2, '0')} ${suffix}`;
-      })
 
       const layout = {
         xaxis: {
-          // range: [0 - xMargin, 6 + xMargin],
           showgrid: false,
           zeroline: false,
           showline: false,
-          tickvals: Array.from({ length: weeksBetween }, (_, i) => i),
-          // tickvals: [0, 1, 2, 3, 4, 5, 6],
-          // ticktext: daysAsStrings,
           ticks: '',
-          // side: 'top',
           scaleanchor: 'y',
         },
         yaxis: {
-          // range: [0 - yMargin, (periodCount - 1) + yMargin],
+          range: [-0.5, 6.5],
           showgrid: false,
           zeroline: false,
           showline: false,
@@ -87,7 +50,7 @@ function GitHub({ tweetTimes, markerSize, plotWidth, plotHeight, timeZone }) {
         },
         width: plotWidth,
       };
-
+      
       var data = [
         {
           z: daysOfYear,
@@ -95,7 +58,7 @@ function GitHub({ tweetTimes, markerSize, plotWidth, plotHeight, timeZone }) {
           xgap: 3,
           ygap: 3,
           colorscale: [
-            [0, 'rgba(145, 47, 192, 0)'],
+            [0, 'rgba(0, 0, 0, 0.3)'],
             [1.0, 'rgba(145, 47, 192, 1)']
           ],
           showscale: false,
